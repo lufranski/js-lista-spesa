85f19f5be20d134abe6cab6b4b266f4e1a304fea
lufranski <lufranski@gmail.com>
2022-11-14 14:58:16 +0100

    For Cicle - console.log

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Lista della spesa
+
+// Settare l'array contenente le cose da comprare
+let listaSpesa = ['Latte' , 'Acqua' , 'Uova' , 'Pane' , 'Biscotti'];
+
+console.log(listaSpesa);
+
+// Stampare gli elementi tramite ciclo for
+for(let i = 0; i < listaSpesa.length; i++){
+
+    console.log(listaSpesa[i]);
+
+}
+// Stampare gli elementi tramite ciclo while
