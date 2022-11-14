f33b9345c6055b93c83ae49a7e3ea69b5c3ec768
lufranski <lufranski@gmail.com>
2022-11-14 15:08:56 +0100

    Bonus start

---
+++ bonus/js/main.js
@@ -0,0 +1,33 @@
+// Lista della spesa
+
+// Settare l'array contenente le cose da comprare
+let listaSpesa = ['Latte' , 'Acqua' , 'Uova' , 'Pane' , 'Biscotti'];
+const container = document.getElementById('wishlist');
+
+console.log(listaSpesa , container);
+
+// Stampare gli elementi tramite ciclo for
+// for(let i = 0; i < listaSpesa.length; i++){
+
+//     console.log(listaSpesa[i]);
+
+//     container.innerHTML += `
+//         <div>${listaSpesa[i]}</div>
+//     `;
+
+// }
+
+// Stampare gli elementi tramite ciclo while
+let i = 0;
+
+while(i < listaSpesa.length){
+
+    console.log(listaSpesa[i]);
+
+    container.innerHTML += `
+        <div>${listaSpesa[i]}</div>
+    `;
+    
+    i++;
+    
+}
