11d69a7afc4eb2d3239eff19d20ad702db62fac9
lufranski <lufranski@gmail.com>
2022-11-14 15:02:27 +0100

    For Cicle - inner.Html

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@
 
 // Settare l'array contenente le cose da comprare
 let listaSpesa = ['Latte' , 'Acqua' , 'Uova' , 'Pane' , 'Biscotti'];
+const container = document.getElementById('wishlist');
 
-console.log(listaSpesa);
+console.log(listaSpesa , container);
 
 // Stampare gli elementi tramite ciclo for
 for(let i = 0; i < listaSpesa.length; i++){
 
     console.log(listaSpesa[i]);
+
+    container.innerHTML += `
+        <div>${listaSpesa[i]}</div>
+    `;
 
 }
 // Stampare gli elementi tramite ciclo while
